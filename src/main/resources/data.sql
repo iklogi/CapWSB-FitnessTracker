df682d2c8d438623b551986938600681c55cf707
mltr11 <mateusz.konopka8@gmail.com>
2025-05-24 13:34:51 +0200

    Zmiany

---
+++ src/main/resources/data.sql
@@ -0,0 +1,17 @@
+INSERT INTO users (id, birthdate, email, first_name, last_name)
+VALUES (1, '1990-01-01', 'jan.kowalski@example.com', 'Jan', 'Kowalski');
+
+INSERT INTO users (id, birthdate, email, first_name, last_name)
+VALUES (2, '1985-05-15', 'anna.nowak@example.com', 'Anna', 'Nowak');
+
+INSERT INTO users (id, birthdate, email, first_name, last_name)
+VALUES (3, '1992-03-22', 'piotr.wozniak@example.com', 'Piotr', 'Woźniak');
+
+INSERT INTO users (id, birthdate, email, first_name, last_name)
+VALUES (4, '1988-11-30', 'kasia.kwiatkowska@example.com', 'Katarzyna', 'Kwiatkowska');
+
+INSERT INTO users (id, birthdate, email, first_name, last_name)
+VALUES (5, '1995-07-08', 'tomasz.lewandowski@example.com', 'Tomasz', 'Lewandowski');
+
+INSERT INTO users (id, birthdate, email, first_name, last_name)
+VALUES (6, '1979-09-14', 'ewa.zielinska@example.com', 'Ewa', 'Zielińska');
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,7 @@
+CREATE TABLE users (
+                       id INT PRIMARY KEY,
+                       birthdate DATE,
+                       email VARCHAR(255),
+                       first_name VARCHAR(255),
+                       last_name VARCHAR(255)
+);
