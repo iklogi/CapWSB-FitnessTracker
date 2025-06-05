b7658b566aec90439da5e2c31b1efd55d330ec18
mltr11 <mateusz.konopka8@gmail.com>
2025-06-05 20:11:15 +0200

    zmiana

--- src/main/java/pl/wsb/fitnesstracker/user/api/UserNotFoundException.java
+++ src/main/java/pl/wsb/fitnesstracker/user/api/UserNotFoundException.java
@@ -1,19 +1,7 @@
 package pl.wsb.fitnesstracker.user.api;
 
-import pl.wsb.fitnesstracker.exception.api.NotFoundException;
-
-/**
- * Exception indicating that the {@link User} was not found.
- */
-@SuppressWarnings("squid:S110")
-public class UserNotFoundException extends NotFoundException {
-
-    private UserNotFoundException(String message) {
+public class UserNotFoundException extends RuntimeException {
+    public UserNotFoundException(String message) {
         super(message);
     }
-
-    public UserNotFoundException(Long id) {
-        this("User with ID=%s was not found".formatted(id));
-    }
-
 }
